3225f411028d3372b3bea03f3425feae18f2686b
JimyRianos <jimy.rianos01@unicatolica.edu.co>
2025-02-23 14:23:21 -0500

    Update Ordenar.py

--- Ordenar.py
+++ Ordenar.py
@@ -1,17 +1,20 @@
-def ordenar_por_insercion(lista):
-    tamaño = len(lista)
+def insercion_ordenar(lista):
+    n = len(lista)
+    i = 1
     
-    for i in range(1, tamaño):
-        valor_actual = lista[i]
+    while i < n:
+        temp = lista[i]
         j = i - 1
         
-        while j >= 0 and lista[j] > valor_actual:
+        while j >= 0 and lista[j] > temp:
             lista[j + 1] = lista[j]
             j -= 1
         
-        lista[j + 1] = valor_actual
+        lista[j + 1] = temp
+        i += 1
 
 # Ejemplo de uso
-datos = [5, 3, 8, 1, 2, 7]
-ordenar_por_insercion(datos)
-print("Lista ordenada:", datos)
+lista = [5, 3, 8, 1, 2, 7]
+insercion_ordenar(lista)
+print("Lista ordenada:", lista)
+
